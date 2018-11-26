457f7810f6c244a04a2b060ec8a7b017547ccc4e
zaikin123 <43007186+zaikin123@users.noreply.github.com>
2018-11-26 13:12:03 +0200

    Lab #6
    1.  Знайти усі досконалі числа в діапазоні від 1 до n. Число називається досконалим, якщо воно дорівнює сумі усіх своїх дільників за виключенням себе самого.

---
+++ Lab_6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+public void real(int m)
+{
+	int i, j, s, l;
+	for (i = 2; i < m; i++)
+	{
+		s = 0;
+		for (j = 1; j < i; j++)
+			if (i%j == 0)
+				s += j;
+		if (s == i)
+			cout << i << '\n';
+
+	}
+}
+
+
+int main()
+{
+	int m;
+	cin >> m;
+	real(m);
+}
